b1c8b0869684948613297cde8fde0745e11c738a
Eduardo David Flores Rojas <davideduardo001@gmail.com>
2021-04-08 01:11:34 -0500

    Implementacion01

--- javascript/script.js
+++ javascript/script.js
@@ -1,44 +1,48 @@
-function sismReplace(){
-    var var01 = document.getElementById("info");
-    var var02 = document.getElementById("status");
-    var var03 = document.getElementById("config");
-    console.log(var01.style.display);
-        var01.style.display = "inline-block";
-        var02.style.display = "none";
-        var03.style.display = "none";
-    console.log("info");
-}
-function statReplace(){
-    var var01 = document.getElementById("info");
-    var var02 = document.getElementById("status");
-    var var03 = document.getElementById("config");
-    console.log(var02.display);
-        var01.style.display = "none";
-        var02.style.display = "inline-block";
-        var03.style.display = "none";
-    console.log("status");
-}
-function confReplace(){
-    var var01 = document.getElementById("info");
-    var var02 = document.getElementById("status");
-    var var03 = document.getElementById("config");
-        var01.style.display = "none";
-        var02.style.display = "none";
-        var03.style.display = "inline-block";
-    console.log("config");
-}
+/* Change window functions */
+  function sismReplace(){
+      var var01 = document.getElementById("info");
+      var var02 = document.getElementById("status");
+      var var03 = document.getElementById("config");
+      console.log(var01.style.display);
+          var01.style.display = "inline-block";
+          var02.style.display = "none";
+          var03.style.display = "none";
+      console.log("info");
+  }
+  function statReplace(){
+      var var01 = document.getElementById("info");
+      var var02 = document.getElementById("status");
+      var var03 = document.getElementById("config");
+      console.log(var02.display);
+          var01.style.display = "none";
+          var02.style.display = "inline-block";
+          var03.style.display = "none";
+      console.log("status");
+  }
+  function confReplace(){
+      var var01 = document.getElementById("info");
+      var var02 = document.getElementById("status");
+      var var03 = document.getElementById("config");
+          var01.style.display = "none";
+          var02.style.display = "none";
+          var03.style.display = "inline-block";
+      console.log("config");
+  }
 
 /* Sticky Control */
+  
+  window.onscroll = function(){stickBar()};
 
-window.onscroll = function() {myFunction()};
-    
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+  
+  
+  function stickBar() {
+    var sticky = document.getElementById("navbar").offsetTop;
+    console.log(window.pageYOffset)
+    console.log(sticky)
+    console.log(document.getElementById("content").offsetTop)
+    if (window.pageYOffset >= 111) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
+    }
   }
-}
