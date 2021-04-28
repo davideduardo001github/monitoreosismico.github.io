42dc8a23cdac652b78fef12636416f3c1986d0a9
Eduardo David Flores Rojas <davideduardo001@gmail.com>
2021-04-27 20:35:11 -0500

    Adición de opciones de interactividad
    Se automatizó proceso de coloreado en el mapa y en despliegue de información

--- javascript/script.js
+++ javascript/script.js
@@ -1,66 +1,91 @@
-/* Change window functions */
-  function sismReplace(){
-      var var01 = document.getElementById("info");
-      var var02 = document.getElementById("status");
-      var var03 = document.getElementById("config");
-      var var04 = document.getElementById("about");
-      console.log(var01.style.display);
-          var01.style.display = "inline-block";
-          var02.style.display = "none";
-          var03.style.display = "none";
-          var04.style.display = "none";
-      console.log("info");
-  }
-  function statReplace(){
-      var var01 = document.getElementById("info");
-      var var02 = document.getElementById("status");
-      var var03 = document.getElementById("config");
-      var var04 = document.getElementById("about");
-      console.log(var02.display);
-          var01.style.display = "none";
-          var02.style.display = "inline-block";
-          var03.style.display = "none";
-          var04.style.display = "none";
-      console.log("status");
-  }
-  function confReplace(){
-      var var01 = document.getElementById("info");
-      var var02 = document.getElementById("status");
-      var var03 = document.getElementById("config");
-      var var04 = document.getElementById("about");
-          var01.style.display = "none";
-          var02.style.display = "none";
-          var03.style.display = "inline-block";
-          var04.style.display = "none";
-      console.log("config");
-  }
-  function aboutReplace(){
+
+/* FUNCIONES DE CAMBIO DE PESTAÑA */
+/* Esta función se encarga de hacer el cambio entre las pantallas sin tener que recargar la página*/
+  function windowSwitch(input01)
+  {
+    console.log("windowSwitch-Enabled")
     var var01 = document.getElementById("info");
     var var02 = document.getElementById("status");
     var var03 = document.getElementById("config");
     var var04 = document.getElementById("about");
+    console.log(input01);
+    switch (input01) 
+    {
+      case 1:
+        var01.style.display = "inline-block";
+        var02.style.display = "none";
+        var03.style.display = "none";
+        var04.style.display = "none";
+        break;
+      case 2:
+        var01.style.display = "none";
+        var02.style.display = "inline-block";
+        var03.style.display = "none";
+        var04.style.display = "none";
+        break;
+      case 3:
+        var01.style.display = "none";
+        var02.style.display = "none";
+        var03.style.display = "inline-block";
+        var04.style.display = "none";
+        break;
+      case 4:
         var01.style.display = "none";
         var02.style.display = "none";
         var03.style.display = "none";
         var04.style.display = "inline-block";
-    console.log("about");
-}
-
-
-/* Sticky Control */
-  
-  /* window.onscroll = function(){stickBar()};
-
-  
-  
-  function stickBar() {
-    var sticky = document.getElementById("navbar").offsetTop;
-    console.log(window.pageYOffset)
-    console.log(sticky)
-    console.log(document.getElementById("content").offsetTop)
-    if (window.pageYOffset >= 111) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
+        break;
     }
-  } */
+  }
+
+  /* FUNCIONES PARA EL MAPA */
+
+  /* Función engargada de el cambio de información en el cuadro de despliegue encima del mapa */
+  function changeInfoMap(nameID, estatusID, idID, lecturaID)
+  {
+    document.getElementById("nameField").innerText = document.getElementById(nameID).innerText;
+    document.getElementById("estatusField").innerText = document.getElementById(estatusID).innerText;
+    document.getElementById("idField").innerText = document.getElementById(idID).innerText;
+    document.getElementById("timeField").innerText = document.getElementById(lecturaID).innerText;
+  }
+
+  /* Función encargada del cambio de colores en el mapa */
+  function colorMap(stateID,mapPath)
+  {
+    console.log("ColorChange-Enabled")
+    var var01 = document.getElementById(stateID).innerText;
+    var var02 = document.getElementById(mapPath);
+    console.log(var01);
+    console.log(var02);
+    switch (var01)
+    {
+      case "Activo":
+        console.log("Active-Case");
+        var02.style.fill = "rgb(119, 207, 124)";
+        break;
+      case "Inactivo":
+        console.log("Inactive-Case");
+        var02.style.fill = "rgb(219, 49, 77)";
+        break;
+      case "Requiere atención":
+        console.log("Atention-Case");
+        var02.style.fill = "hsl(46, 100%, 67%)";
+        break;
+    }
+  }
+
+  function firstLoad()
+  {
+    /* Coloreado del mapa con estados de primera vez */
+    
+    for (let i = 1; i <= 16; i++) {
+      let pathID = i.toString()+'Path';
+      let statusID = i.toString()+'Status';
+      console.log(pathID);
+      console.log(statusID);
+      colorMap('2Status','2Path');
+      colorMap(statusID,pathID);
+    }
+    console.log("StartPage");
+    
+  }
